5757c931834c917f0e600e9ee6d1fb37c5781885
Hwa Yeon Lee <58550449+tacoculbreth@users.noreply.github.com>
2023-06-10 19:02:47 -0700

    Update myhomer.py

--- myhomer/myhomer.py
+++ myhomer/myhomer.py
@@ -1,97 +1,155 @@
 #!/usr/bin/env python3
-
 import argparse
+import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
+from myutils import compute_pfm, compute_pwm, ScoreSeq, extract_motifs, GetThreshold, RandomSequence, compute_pvalue, compute_nucleotide_frequencies
 import pandas as pd
+import logomaker
+import datetime
+import time
 import numpy as np
 import os
 
 def main():
+    print(f"Current working directory: {os.getcwd()}")
+    print(sys.argv)
 
-    #----------------------------------------------------------------
-    #
-    # Input, Output, Arguments
-    #
-    #----------------------------------------------------------------
-    
     parser = argparse.ArgumentParser(
         prog="myhomer",
         description="Command-line script to perform motif finding against set of known motifs"
     )
 
-    # Input
     parser.add_argument("peaks_file", help="HOMER Peak file", type=str, metavar="FILE")
     
     parser.add_argument("fasta_ref", \
                         help="faidx indexed reference sequence file", \
                         type=str, metavar="FILE")
 
-    # Output
     parser.add_argument("output_dir", help="Output directory", type=str)
     
-    # Parse args
     args = parser.parse_args()
 
-    #print("Current working directory:", os.getcwd())
-    #print("Absolute path to peaks file:", os.path.abspath(args.peaks_file))
-
-    # Load Fasta
     if not os.path.exists(args.fasta_ref):
         raise FileNotFoundError(f"{args.fasta_ref} does not exist")
 
     fasta_sequences = SeqIO.to_dict(SeqIO.parse(args.fasta_ref, 'fasta'))
 
-    # Get the absolute path of the peaks file
+    # Print variable settings
     peaks_file = args.peaks_file
+    fasta_ref = args.fasta_ref
+    output_dir = args.output_dir
+    print("\n====================Variables setting====================")
+    print(f"\nHOMER Peaks File: {peaks_file}")
+    print(f"\nReference Genome: {fasta_ref}")
+    print(f"\nOutput Directory: {output_dir}")
+    print("=========================================================\n")
 
     # Read HOMER peaks file, ignoring the first 39 lines and treating line 40 as column headers
     peaks = pd.read_csv(peaks_file, sep='\t', skiprows=39, header=0)
 
-    #----------------------------------------------------------------
-    #
-    # Extract Sequences from the start to end positions of each peak
-    #
-    #----------------------------------------------------------------
+    # MEME-formatted motif database
+    motif_db = "../motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme"
+    motifs = extract_motifs(motif_db)
 
+    # -------------------------------------------------------------
+    #
+    # Run
+    #
+    # -------------------------------------------------------------
+
+    # Extract 100bp sequences centered on the midpoint of each peak
     sequences = []
     for index, row in peaks.iterrows():
-        seq_id = row['chr']  # Now reading the 'chr' column
+        seq_id = row['chr']
         start = row['start']
         end = row['end']
-        sequences.append(str(fasta_sequences[seq_id].seq[start:end]))
+        mid_point = start + ((end - start) // 2)
+        start_seq = max(mid_point - 50, 0)
+        end_seq = min(mid_point + 50, len(fasta_sequences[seq_id].seq))
+        sequences.append(str(fasta_sequences[seq_id].seq[start_seq:end_seq]))
+
+    output_filename = "../benchmark/Klf4/sea_results/sequences.txt"
+
+    # Write the sequences to the file
+    with open(output_filename, 'w') as f:
+        for sequence in sequences:
+            f.write("%s\n" % sequence)
+
+    nucleotide_frequencies = compute_nucleotide_frequencies(sequences)
     
-    #----------------------------------------------------------------
-    #
-    # Create PFM
-    #
-    #----------------------------------------------------------------
+    # Compute PFM, PWM
+    pfm = compute_pfm(sequences)
+    pwm = compute_pwm(pfm, len(sequences))
 
-    # Initialize position frequency matrix with zeros
-    pfm = np.zeros((4, len(sequences[0])))
+    # Generate a null distribution of scores
+    numsim = 10000
+    n = len(next(iter(motifs.values()))[0])
+    null_scores = [ScoreSeq(pwm, RandomSequence(n, nucleotide_frequencies)) for i in range(numsim)]
 
-    # DNA base to index mapping
-    base_to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+    # Calculate score threshold
+    pval_threshold = 0.01  # You can adjust this based on your desired level of significance
+    thresh = GetThreshold(null_scores, pval_threshold)
 
-    for sequence in sequences:
-        for i, base in enumerate(sequence):
-            pfm[base_to_index[base]][i] += 1
-    
-    # Print PFM
-    print("Position Frequency Matrix (PFM):")
-    print(pfm)
+    # Find the top motifs and append them to the motifs_sorted list
+    motifs_scores = []
+    for motif_name, motif_pwm in motifs.items():
+        # Transpose the motif_pwm
+        motif_pwm = np.transpose(motif_pwm)
 
-    #----------------------------------------------------------------
-    #
-    # Create PWM
-    #
-    #----------------------------------------------------------------
+        scores = [ScoreSeq(motif_pwm, seq) for seq in sequences]
+        avg_score = np.mean(scores)
+        pval = compute_pvalue(scores, null_scores)
 
-    pwm = np.log2((pfm+1) / (len(sequences)+4))  # Add pseudocounts to avoid taking log of zero
+        motifs_scores.append((motif_name, avg_score, pval))
 
-    # Print PWM
-    print("Position Weight Matrix (PWM):")
-    print(pwm)
+    # Sort the motifs by score in descending order
+    motifs_scores.sort(key=lambda x: x[1], reverse=True)
+
+    # Get the top 10 motifs
+    top_motifs = motifs_scores[:10]
+
+    # Create output directory if it does not exist
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(os.path.join(output_dir, "motifs_enrichment_results.txt"), "w") as pwm_file:
+        # Print the top 10 motifs to the file
+        for i, (motif_name, avg_score, pval) in enumerate(top_motifs):
+            pwm_file.write(f"{motif_name}, Score: {avg_score}, P-value: {pval}\n")
+
+            # Get the PWM for the current motif
+            motif_pwm = motifs[motif_name]
+
+            # Transpose the motif_pwm
+            motif_pwm = np.transpose(motif_pwm)
+
+            # Generate and save sequence logo
+            pwm_df = pd.DataFrame(motif_pwm, columns=['A', 'C', 'G', 'T'])
+            logo = logomaker.Logo(pwm_df)
+            logo.fig.savefig(os.path.join(output_dir, f"{motif_name}_logo.png"))
+
+            # Write motif_pwm to the file
+            np.savetxt(pwm_file, motif_pwm, delimiter='\t', fmt='%.3f')
+
+            pwm_file.write('\n')  # Add a newline between motifs
 
 if __name__ == "__main__":
+    print("\nRunning myhomer ..... ")
+    current_datetime = datetime.datetime.now()
+    start_time = time.time()
+
     main()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    formatted_date, formatted_time = formatted_datetime.split(" ")
+
+    print("\nDone!!!")
+    print("\nDate:", formatted_date)
+    print("Time:", formatted_time)
+    m, s = divmod(elapsed_time, 60)
+    h, m = divmod(m, 60)
+    print(f"Elapsed Time: {int(h)}h:{int(m)}m:{s:.2f}s")
